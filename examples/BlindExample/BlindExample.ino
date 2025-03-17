34a847bb1c8818a65adcb5584466067960ef458d
Oskar Rasienski <oskar.rasienski@gmail.com>
2025-03-17 14:10:23 +0100

    fixed tabs

--- examples/BlindExample/BlindExample.ino
+++ examples/BlindExample/BlindExample.ino
@@ -5,18 +5,18 @@ MazeSolver mazeSolver;
 int main() {
     constexpr int w = 7;
     constexpr int h = 7;
-	mazeSolver.init(
-		4.,  // 4cm between cells
-		20., // 20cm wide cells
-		20., // 20cm high cells
-		w * 2 - 1, // make the supermaze (2x-1) (13x13)
-		h * 2 - 1,
-		{ w-1,h-1 }, // middle of the supermaze startPos(6,6)
-		{ -1,-1 }, // random value, doesn't matter
+    mazeSolver.init(
+        4.,  // 4cm between cells
+        20., // 20cm wide cells
+        20., // 20cm high cells
+        w * 2 - 1, // make the supermaze (2x-1) (13x13)
+        h * 2 - 1,
+        { w-1,h-1 }, // middle of the supermaze startPos(6,6)
+        { -1,-1 }, // random value, doesn't matter
         true // blind
-	);
+    );
 
-   	mazeSolver.markWall(mazeSolver.getStartPos(), 10, CompassDir::East);
+    mazeSolver.markWall(mazeSolver.getStartPos(), 10, CompassDir::East);
     vec2<int> nm = mazeSolver.getNextMove(0); // returns (6,5), which means one up 
     mazeSolver.setCurrPos({6,5});
     
